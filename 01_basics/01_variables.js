f2b6af740947ee845aefe80625bb6c6b0c640bfa
H-101 <121651068+H-101@users.noreply.github.com>
2024-11-27 08:40:28 +0000

    more

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const account_Id=14453
+let accountEmail='hhau@gmail.com'
+var accountPassword='12345'
+accountCity='jaipur'
+console.log(account_Id);
+console.table([account_Id,accountEmail,accountPassword,accountCity]);
+
